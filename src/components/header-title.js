48f09bc38a8da498d2445cba325cf5e8ec09a48c
Román Gutierrez <roman.gutierrez@gmail.com>
2021-07-13 16:22:08 -0300

    Add short-name field as the header location info

--- src/components/header-title.js
+++ src/components/header-title.js
@@ -11,112 +11,101 @@
  * limitations under the License.
  **/
 
-import React from 'react'
+import React from "react";
 import URI from "urijs";
-import { daysBetweenDates, getFormatedDate } from '../utils/helpers';
+import { daysBetweenDates, getFormatedDate } from "../utils/helpers";
 
 class HeaderTitle extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {};
 
-        };
+    this.handleEventDateLocation = this.handleEventDateLocation.bind(this);
+  }
 
-        this.handleEventDateLocation = this.handleEventDateLocation.bind(this);
-
-    }
-
-    handleEventDateLocation() {
-      let {summit} = this.props;
-      if(summit && summit.locations && summit.start_date && summit.end_date && summit.time_zone_id) {
-        let location = summit.locations.filter(l => l.class_name === "SummitVenue").find(l => l.is_main === true);
-        let dateRange = daysBetweenDates(summit.start_date, summit.end_date, summit.time_zone_id);
-        let summitDate = '';
-        let summitLocation = '';
-        if(dateRange.length > 1) {
-          let startDate = getFormatedDate(dateRange[0], summit.time_zone_id);
-          let endDate = getFormatedDate(dateRange[dateRange.length-1], summit.time_zone_id);
-          let startYear = startDate.substring(startDate.length, startDate.length-4);
-          let endYear = endDate.substring(endDate.length, endDate.length-4);
-          if (startYear === endYear) {
-            startDate = startDate.substring(0, startDate.length-6)
-          }
-          let startMonth = startDate.split(' ')[0];
-          let endMonth = endDate.split(' ')[0];
-          if(startMonth === endMonth && startYear === endYear) { 
-            endDate = endDate.substr(endDate.indexOf(" ") + 1);
-          }          
-          summitDate = `${startDate} - ${endDate}`;
-        } else {
-          summitDate = getFormatedDate(summit.start_date, summit.time_zone_id);        
+  handleEventDateLocation() {
+    let { summit } = this.props;
+    if (summit && summit.locations && summit.start_date && summit.end_date && summit.time_zone_id) {
+      let location = summit.locations.filter((l) => l.class_name === "SummitVenue").find((l) => l.is_main === true);
+      let dateRange = daysBetweenDates(summit.start_date, summit.end_date, summit.time_zone_id);
+      let summitDate = "";
+      let summitLocation = "";
+      if (dateRange.length > 1) {
+        let startDate = getFormatedDate(dateRange[0], summit.time_zone_id);
+        let endDate = getFormatedDate(dateRange[dateRange.length - 1], summit.time_zone_id);
+        let startYear = startDate.substring(startDate.length, startDate.length - 4);
+        let endYear = endDate.substring(endDate.length, endDate.length - 4);
+        if (startYear === endYear) {
+          startDate = startDate.substring(0, startDate.length - 6);
         }
-        if(location) {
-          summitLocation = `${location.city}, ${location.country} `;
+        let startMonth = startDate.split(" ")[0];
+        let endMonth = endDate.split(" ")[0];
+        if (startMonth === endMonth && startYear === endYear) {
+          endDate = endDate.substr(endDate.indexOf(" ") + 1);
         }
-        if(summitLocation !== '') {
-          return (
-            <React.Fragment>
-              {summitDate}
-              <div className="mb-2"></div>
-              {summitLocation}
-            </React.Fragment>
-          );
-        } else if (summitDate) {
-          return (
-            <React.Fragment>
-              {summitDate}                        
-            </React.Fragment>
-          );
-        }
+        summitDate = `${startDate} - ${endDate}`;
       } else {
-        return null;
-      }                  
-    }
-
-    getHeaderTitle() {
-        const {summit} = this.props;
-        let url = URI(window.location.href);
-        let location = url.pathname();
-        let purchaseLocation = '/register/';
-
-        if (summit && location.match(purchaseLocation)) {
-            return (
-                <>
-                    <h4>
-                        {summit.name ? summit.name : 'Registration'}
-                    </h4>
-                    <h5>{location.match(purchaseLocation) ? this.handleEventDateLocation() : ''}</h5>
-                </>
-            );
-        } else {
-            return (<h4 className="registration-title">Registration</h4>);
-        }
-
-    };
-
-
-    render() {
-        let {summit} = this.props;
-        let url = URI(window.location.href);
-        let location = url.pathname();
-        let purchaseLocation = '/register/';
-
+        summitDate = getFormatedDate(summit.start_date, summit.time_zone_id);
+      }
+      if (location) {
+        summitLocation = location.short_name
+          ? `${location.short_name}, ${location.city}, ${location.country} `
+          : `${location.city}, ${location.country} `;
+      }
+      if (summitLocation !== "") {
         return (
-            <div className={`${summit.logo ? 'header-title' : 'header-title header-title--no-logo'}`}>
-                <div className="summit-info">
-                    {location.match(purchaseLocation) && summit && summit.logo &&
-                        <a href="/a">
-                        <img className="summit-logo" src={summit.logo} alt={summit.name ? summit.name : ''}/>
-                        </a>
-                    }
-                    <div className={`${summit.logo ? 'summit-text' : 'summit-text--no-logo'}`}>
-                        {this.getHeaderTitle()}
-                    </div>
-                </div>
-            </div>
+          <React.Fragment>
+            {summitDate}
+            <div className="mb-2"></div>
+            {summitLocation}
+          </React.Fragment>
         );
+      } else if (summitDate) {
+        return <React.Fragment>{summitDate}</React.Fragment>;
+      }
+    } else {
+      return null;
     }
+  }
+
+  getHeaderTitle() {
+    const { summit } = this.props;
+    let url = URI(window.location.href);
+    let location = url.pathname();
+    let purchaseLocation = "/register/";
+
+    if (summit && location.match(purchaseLocation)) {
+      return (
+        <>
+          <h4>{summit.name ? summit.name : "Registration"}</h4>
+          <h5>{location.match(purchaseLocation) ? this.handleEventDateLocation() : ""}</h5>
+        </>
+      );
+    } else {
+      return <h4 className="registration-title">Registration</h4>;
+    }
+  }
+
+  render() {
+    let { summit } = this.props;
+    let url = URI(window.location.href);
+    let location = url.pathname();
+    let purchaseLocation = "/register/";
+
+    return (
+      <div className={`${summit.logo ? "header-title" : "header-title header-title--no-logo"}`}>
+        <div className="summit-info">
+          {location.match(purchaseLocation) && summit && summit.logo && (
+            <a href="/a">
+              <img className="summit-logo" src={summit.logo} alt={summit.name ? summit.name : ""} />
+            </a>
+          )}
+          <div className={`${summit.logo ? "summit-text" : "summit-text--no-logo"}`}>{this.getHeaderTitle()}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default HeaderTitle;
