06ea6bd1f58a620449ac3139b044fb3e7a0ea1e2
Tomás Castillo <tcastilloboireau@gmail.com>
2021-12-29 16:00:21 -0300

    Add new font, change default fonf family
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux'
 import store, {persistor} from './store';
 import { PersistGate } from 'redux-persist/integration/react'
 import 'font-awesome/css/font-awesome.css';
-import './styles/general.less';
 import 'sweetalert2/dist/sweetalert2.css';
 import './styles/utilities/colors.css';
+import './styles/general.less';
 
 ReactDOM.render(
     <Provider store={store}>
