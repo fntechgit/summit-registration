a58ae3f879029f42decc3941efd2300189f4f993
smarcet <smarcet@gmail.com>
2022-01-17 10:35:36 -0300

    Add flush state
    Signed-off-by: smarcet <smarcet@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,9 +10,20 @@ import 'sweetalert2/dist/sweetalert2.css';
 import './styles/utilities/colors.css';
 import './styles/general.less';
 
+import { RESET_STATE } from "./actions/base-actions";
+
+const onBeforeLift = () => {
+    const params = new URLSearchParams(window.location.search);
+    const flush = params.has("flushState");
+
+    if (flush) {
+        store.dispatch({ type: RESET_STATE, payload: null });
+    }
+};
+
 ReactDOM.render(
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        <PersistGate onBeforeLift={onBeforeLift} loading={null} persistor={persistor}>
             <App />
         </PersistGate>
     </Provider>,
