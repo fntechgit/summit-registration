e836ce31e314caa7e6fc16548e589263b14acd95
Tomás Castillo <tcastilloboireau@gmail.com>
2022-02-08 16:36:57 -0300

    Adding comments
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/components/ticket-assign-form.js
+++ src/components/ticket-assign-form.js
@@ -89,6 +89,7 @@ class TicketAssignForm extends React.Component {
             showCancel = this.props.showCancel;
         let {extra_questions, input_email} = this.state;
         ticket.disclaimer_accepted = ticket.disclaimer_accepted == null ? false : ticket.disclaimer_accepted;
+        // if the user is purchasing a ticket, allow to edit the extra questions (fromTicketList === undefined && fromOrderList === undefined)
         const allow_extra_questions_edit = (fromTicketList === undefined && fromOrderList === undefined) || ownedTicket && summit.allow_update_attendee_extra_questions;
         return (
             <div className="ticket-assign-form">
