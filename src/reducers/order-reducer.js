c6d67c476c9877cd57c6d979f841df354a1edac4
Tomás Castillo <tomrndom@users.noreply.github.com>
2022-05-03 16:16:45 -0300

    New company input implementation (#117)
    * New company input implementation
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>
    
    * Update uicore version
    
    Signed-off-by: Tomás Castillo <tcastilloboireau@gmail.com>

--- src/reducers/order-reducer.js
+++ src/reducers/order-reducer.js
@@ -13,12 +13,12 @@
 
 import { START_LOADING, STOP_LOADING, LOGOUT_USER } from "openstack-uicore-foundation/lib/actions";
 
-import { 
-    RESET_ORDER, 
-    RECEIVE_ORDER, 
-    CHANGE_ORDER, 
-    VALIDATE_STRIPE, 
-    CREATE_RESERVATION, 
+import {
+    RESET_ORDER,
+    RECEIVE_ORDER,
+    CHANGE_ORDER,
+    VALIDATE_STRIPE,
+    CREATE_RESERVATION,
     CREATE_RESERVATION_SUCCESS,
     CREATE_RESERVATION_ERROR,
     PAY_RESERVATION,
@@ -35,7 +35,10 @@ const DEFAULT_ENTITY = {
     first_name: '',
     last_name: '',
     email: '',
-    company: '',    
+    company: {
+        name: '',
+        id: null
+    },
     billing_country: '',
     billing_address: '',
     billing_address_two: '',
@@ -65,20 +68,20 @@ const DEFAULT_STATE = {
 const orderReducer = (state = DEFAULT_STATE, action) => {
     const { type, payload } = action
 
-    switch(type){
+    switch (type) {
         case LOGOUT_USER:
-            return { ...DEFAULT_STATE, purchaseOrder: {...state.purchaseOrder} };
+            return { ...DEFAULT_STATE, purchaseOrder: { ...state.purchaseOrder } };
             break;
         case START_LOADING:
-            return {...state, loading: true};
+            return { ...state, loading: true };
             break;
         case STOP_LOADING:
-            return {...state, loading: false};
+            return { ...state, loading: false };
             break;
         case RESET_ORDER:
-            let {step} = payload;
-            if(step){
-                return {...DEFAULT_STATE, purchaseOrder: {...DEFAULT_ENTITY, currentStep: step}};
+            let { step } = payload;
+            if (step) {
+                return { ...DEFAULT_STATE, purchaseOrder: { ...DEFAULT_ENTITY, currentStep: step } };
             }
             return DEFAULT_STATE;
             break;
@@ -86,54 +89,54 @@ const orderReducer = (state = DEFAULT_STATE, action) => {
             return state;
             break;
         case CHANGE_ORDER:
-            let {order, errors} = payload;
-            return {...state, purchaseOrder: order, errors: errors};
+            let { order, errors } = payload;
+            return { ...state, purchaseOrder: order, errors: errors };
             break;
         case VALIDATE_STRIPE:
-            let {value} = payload
-            return {...state, stripeForm: value};
+            let { value } = payload
+            return { ...state, stripeForm: value };
             break;
         case CREATE_RESERVATION:
             return state
             break;
         case CREATE_RESERVATION_SUCCESS:
-            let entity = {...payload.response};
-            return {...state, purchaseOrder: {...state.purchaseOrder, reservation: entity, tickets: entity.tickets }, errors: {}, loading: false, loaded: true};
+            let entity = { ...payload.response };
+            return { ...state, purchaseOrder: { ...state.purchaseOrder, reservation: entity, tickets: entity.tickets }, errors: {}, loading: false, loaded: true };
             break;
         case CREATE_RESERVATION_ERROR:
-            let {tickets} = {...state.purchaseOrder};
+            let { tickets } = { ...state.purchaseOrder };
             tickets.map(t => {
-              if(!t.tempId) {
-                const randomNumber = Math.floor(Math.random() * 10000) + 1; 
-                t.tempId = randomNumber;
-                return t;
-              }
+                if (!t.tempId) {
+                    const randomNumber = Math.floor(Math.random() * 10000) + 1;
+                    t.tempId = randomNumber;
+                    return t;
+                }
             });
-            return {...state, purchaseOrder: {...state.purchaseOrder, tickets}};
+            return { ...state, purchaseOrder: { ...state.purchaseOrder, tickets } };
             break;
         case DELETE_RESERVATION:
             return state
             break;
         case DELETE_RESERVATION_SUCCESS:
             let noDiscountTix = [...state.purchaseOrder.tickets];
-            noDiscountTix.map(t => t.discount = 0);            
-            return {...state, purchaseOrder: {...state.purchaseOrder, reservation: {}, tickets: noDiscountTix}}
+            noDiscountTix.map(t => t.discount = 0);
+            return { ...state, purchaseOrder: { ...state.purchaseOrder, reservation: {}, tickets: noDiscountTix } }
         case PAY_RESERVATION:
-            return { ...state, purchaseOrder : { ...state.purchaseOrder, checkout : payload.response}};
+            return { ...state, purchaseOrder: { ...state.purchaseOrder, checkout: payload.response } };
             break;
-        case CLEAR_RESERVATION:{
-            return { ...state, reservation: {}};
+        case CLEAR_RESERVATION: {
+            return { ...state, reservation: {} };
             break;
         }
         case GET_USER_ORDERS:
-            let {data, current_page, total, last_page} = payload.response;
-            return {...state, memberOrders: data, current_page, total, last_page};
+            let { data, current_page, total, last_page } = payload.response;
+            return { ...state, memberOrders: data, current_page, total, last_page };
             break;
         case SELECT_ORDER:
-            return {...state, selectedOrder: payload};
+            return { ...state, selectedOrder: payload };
             break;
         case REFUND_ORDER:
-            return {...state}
+            return { ...state }
         default:
             return state;
             break;
